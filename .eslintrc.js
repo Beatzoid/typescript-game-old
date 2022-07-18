248b9c1189befa9591c1b8d3f0ec423ee635e0df
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-07-18 10:01:41 -0700

    🎉 Initial Commit
    Initial commit with basic ECS system, CI, and configs

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        jest: true,
+        es6: true,
+    },
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    // add your custom rules here
+    rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        "@typescript-eslint/explicit-function-return-type": [
+            "error",
+            {
+                allowExpressions: false,
+                allowTypedFunctionExpressions: true,
+                allowHigherOrderFunctions: true,
+            },
+        ],
+        "no-unused-vars": "off", // note you must disable the base rule as it can report incorrect errors
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                vars: "all",
+                args: "none",
+                ignoreRestSiblings: false,
+            },
+        ],
+        "no-useless-constructor": "off",
+        "@typescript-eslint/no-useless-constructor": "error",
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: true,
+                },
+            },
+            {
+                selector: "variable",
+                format: ["camelCase"],
+            },
+            {
+                selector: "memberLike",
+                modifiers: ["private"],
+                format: ["camelCase"],
+                leadingUnderscore: "require",
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ["*.js"],
+            rules: {
+                "@typescript-eslint/explicit-function-return-type": "off",
+            },
+        },
+    ],
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Typescript Game
+
+A game made using Typescript
+
+# Demo
+
+Soon
+
+# Usage
+
+1. Clone/download project
+2. `yarn install`
+3. `yarn start`
+4. Open link in browser
+
+# Credit
+
+[This](https://levelup.gitconnected.com/gamedev-patterns-and-algorithms-with-typescript-game-loop-part-1-2-699919bb9b71)
